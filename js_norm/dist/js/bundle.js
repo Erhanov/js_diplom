8b92815849d93c90ae2e8fbe11f26694fd894754
Azat <aza.er.01@mail.ru>
2019-04-03 22:05:32 +0600

    final

--- js_norm/dist/js/bundle.js
+++ js_norm/dist/js/bundle.js
@@ -1622,20 +1622,23 @@ function burger() {
   var burger = document.querySelector('.burger'),
       burgerButton = document.querySelector('.burger-menu'),
       a = 0;
-  burger.addEventListener('click', function () {
-    if (a % 2 == 0) {
-      burgerButton.style.display = 'block';
-      burgerButton.classList.add('animated', 'fadeInDown');
-      a++;
-    } else {
-      burgerButton.classList.add('animated', 'fadeOutUp');
-      a++;
-      setTimeout(function () {
-        burgerButton.style.display = 'none';
-        burgerButton.classList.remove('animated', 'fadeOutUp');
-      }, 500);
-    }
-  });
+
+  if (window.innerWidth <= 768) {
+    burger.addEventListener('click', function () {
+      if (a % 2 == 0) {
+        burgerButton.style.display = 'block';
+        burgerButton.classList.add('animated', 'fadeInDown');
+        a++;
+      } else {
+        burgerButton.classList.add('animated', 'fadeOutUp');
+        a++;
+        setTimeout(function () {
+          burgerButton.style.display = 'none';
+          burgerButton.classList.remove('animated', 'fadeOutUp');
+        }, 500);
+      }
+    });
+  }
 }
 
 module.exports = burger;
--- js_norm/src/parts/burger.js
+++ js_norm/src/parts/burger.js
@@ -3,21 +3,29 @@ function burger() {
 		burgerButton = document.querySelector('.burger-menu'),
 		a = 0;
 
-	burger.addEventListener('click', () => {
+
+	if (window.innerWidth <= 768) {
+		burger.addEventListener('click', () => {
 		if (a % 2 == 0) {
-			burgerButton.style.display = 'block';
-			burgerButton.classList.add('animated', 'fadeInDown');
-			a++;
-		} else {
-			burgerButton.classList.add('animated', 'fadeOutUp');
-			a++;
-			setTimeout(function() {
-				burgerButton.style.display = 'none';
-				burgerButton.classList.remove('animated', 'fadeOutUp');
-			}, 500);
-			
-		}
-	});
+				burgerButton.style.display = 'block';
+				burgerButton.classList.add('animated', 'fadeInDown');
+				a++;
+			} else {
+				burgerButton.classList.add('animated', 'fadeOutUp');
+				a++;
+				setTimeout(function() {
+					burgerButton.style.display = 'none';
+					burgerButton.classList.remove('animated', 'fadeOutUp');
+				}, 500);
+				
+			}
+		});
+	}
+
+
+
+
+	
 }
 
 module.exports = burger;
