23b670d3eac87f58b45bfa6def0b528a2808b103
Azat <aza.er.01@mail.ru>
2019-04-04 16:18:18 +0600

    final

--- js_norm/src/parts/burger.js
+++ js_norm/src/parts/burger.js
@@ -5,24 +5,26 @@ function burger() {
 		MainBody = document.getElementsByTagName("body")[0],
 		a = 0;
 
+
 	function checkBurger() {
 		if (window.innerWidth <= 768) {
 			if (a % 2 == 0) {
-					burgerButton.style.display = 'block';
-					burgerButton.classList.add('animated', 'fadeInDown');
-					a++;
-				} else {
-					burgerButton.classList.add('animated', 'fadeOutUp');
-					a++;
-					setTimeout(function() {
-						burgerButton.style.display = 'none';
-						burgerButton.classList.remove('animated', 'fadeOutUp');
-					}, 500);			
-				}
+				burgerButton.style.display = 'block';
+				burgerButton.classList.add('animated', 'fadeInDown');
+				a++;
+			} else {
+				burgerButton.classList.add('animated', 'fadeOutUp');
+				a++;
+				setTimeout(function() {
+					burgerButton.style.display = 'none';
+					burgerButton.classList.remove('animated', 'fadeOutUp');
+				}, 500);			
 			}
+		}
+
 	}
 
-	burger.addEventListener('click', checkBurger)
+	burger.addEventListener('click', checkBurger);
 }
 
 module.exports = burger;
